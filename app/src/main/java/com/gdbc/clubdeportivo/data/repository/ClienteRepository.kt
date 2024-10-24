f6bcad0f5b390d4dae9299116291882b70d75c45
Catriel Escobar <125727012+Catriel-Escobar@users.noreply.github.com>
2024-10-24 16:36:04 -0300

    cliente repo

---
+++ app/src/main/java/com/gdbc/clubdeportivo/data/repository/ClienteRepository.kt
@@ -0,0 +1,8 @@
+package com.gdbc.clubdeportivo.data.repository
+
+import com.gdbc.clubdeportivo.data.database.BDatos
+
+class ClienteRepository(dbHelper:BDatos) {
+	 private val db = dbHelper
+	 
+}
